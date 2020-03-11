7a8ca41b26b946b28cd447bc6681c78d5abb1d84
Adam <adamjackson@pursuit.org>
2020-03-11 11:18:45 -0400

    Moving to Swift UI for music player

--- SoundSpaceMusic/ContentView.swift
+++ SoundSpaceMusic/ContentView.swift
@@ -8,14 +8,36 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView: View {
+//  
+//    var body: some View {
+//        VStack {
+//        Image("coverArt")
+//        .resizable()
+//            .frame(width: 300, height: 300)
+//        
+//        Button(action: {
+//            print("Delete tapped!")
+//        }) {
+//            HStack {
+//                    .font(.title)
+//                Text("Delete")
+//                    .fontWeight(.semibold)
+//                    .font(.title)
+//            }
+//            .padding()
+//            .foregroundColor(.white)
+//            .background(Color.red)
+//            .cornerRadius(CGFloat(40)
+//        }
+//        
+//        }
+//        
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
